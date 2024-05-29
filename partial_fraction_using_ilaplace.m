aef400b36d48cab13f1b2a2dcbccf02317c313fa
karanBeeshm <karanbeeshmrajkumar@gmail.com>
2024-05-29 21:39:57 +0530

    Create partial_fraction_using_ilaplace.m

---
+++ partial_fraction_using_ilaplace.m
@@ -0,0 +1,18 @@
+pkg load symbolic
+syms s
+
+A = [s+2 -1;-1 s+3]
+B = [2/(s+3);0];
+X_a= inv(A)*B
+X_t= ilaplace(X_a)
+
+#t=0:0.05:10;
+#y1=subs(X_t(1),s,t);
+#y2=subs(X_t(2),s,t);
+
+plot([0,10],X_t(1),"ro","markersize",5)
+hold on
+plot([0,10],X_t(1),"bo","markersize",5)
+xlabel("t")
+legend("x1","x2")
+ylabel("x(t)")
