9d575e52c32b1ebd62550103f28c73fc649628a3
karanBeeshm <karanbeeshmrajkumar@gmail.com>
2024-05-30 10:14:18 +0530

    Update partial_fraction_using_ilaplace.m

--- partial_fraction_using_ilaplace.m
+++ partial_fraction_using_ilaplace.m
@@ -1,18 +1,18 @@
 pkg load symbolic
 syms s
 
-A = [s+2 -1;-1 s+3]
+A = [s+2 -1;-1 s+3];
 B = [2/(s+3);0];
-X_a= inv(A)*B
-X_t= ilaplace(X_a)
+X_a= inv(A)*B;
+X_t= ilaplace(X_a);
 
-#t=0:0.05:10;
-#y1=subs(X_t(1),s,t);
-#y2=subs(X_t(2),s,t);
+t=0:0.05:10;
+y1=function_handle(X_t(1));
+y2=function_handle(X_t(2));
 
-plot([0,10],X_t(1),"ro","markersize",5)
+plot(t,y1(t),"ro","markersize",5)
 hold on
-plot([0,10],X_t(1),"bo","markersize",5)
+plot(t,y2(t),"bo","markersize",5)
 xlabel("t")
 legend("x1","x2")
 ylabel("x(t)")
