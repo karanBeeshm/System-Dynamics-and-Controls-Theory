a319d9fe2be1f49275c1edc4d89f05f112bb0d1b
karanBeeshm <karanbeeshmrajkumar@gmail.com>
2024-06-07 11:49:15 +0530

    multi_input_multi_output_state_space

---
+++ state_space_ode45_3.m
@@ -0,0 +1,27 @@
+#________________________________#
+#   |                       |    #
+# spring                    |    #
+#   |                       |    #
+# |Mass|                  spring #
+#   |                       |    #
+# spring                    |    #
+#   |                       |    #
+# ||||||||||||MASS|||||||||||||| #
+#                                #
+
+tspan = [0 10];
+x0 = [0 0 0 0];
+
+[t, x] = ode45(@fun_3,tspan,x0);
+figure(1)
+plot(t,x(:,1))
+hold on
+plot(t,x(:,3))
+legend('M1','M2')
+title("Displacement")
+figure(2)
+plot(t,x(:,2))
+hold on
+plot(t,x(:,4))
+legend('M1','M2')
+title("Velocity")
